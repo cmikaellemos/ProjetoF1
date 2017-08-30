ad5344c1080cce14c8360da21358a108c3ff4050
Mikael Lemos <mikaellemos@alu.ufc.br>
2017-08-29 22:23:49 -0300

    fixed infowindow

--- circuits/circuits.js
+++ circuits/circuits.js
@@ -12,35 +12,50 @@ function initialize() {
    map = new google.maps.Map(document.getElementById("map"), options);
 }
 
-initialize();
+
+var pontos;
 
 function carregarPontos() {
+    
+    const ponto = pontos.MRData.CircuitTable.Circuits;
 
-    $.getJSON("http://ergast.com/api/f1/circuits.json?limit=300", function(pontos) {
-        console.log(pontos);
-        $.each(pontos.MRData.CircuitTable.Circuits, function(index, ponto) {
-
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(ponto.Location.lat, ponto.Location.long),
-                title: ponto.circuitName + ", " + ponto.Location.locality + " - " + ponto.Location.country,
-
-                map: map
-            });
-
-            var contentString = ponto.circuitName + ", " + ponto.Location.locality + " - " + ponto.Location.country + '<br>' +
-            '<a href="' + ponto.url + '" target="_blank"> Mais informações </a>';
-
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-
-            marker.addListener('click', function() {
-                infowindow.open(map, marker);
-            });
-        });
-
-    });
-
+    for (var i = 0; i < pontos.MRData.total; i++) {
+        loadMarker(ponto[i]);
+    }
+    
 }
 
-carregarPontos();
+function loadMarker(ponto){
+    var contentString = ponto.circuitName + ", " + ponto.Location.locality + " - " + ponto.Location.country + '<br>' +
+    '<a href="' + ponto.url + '" target="_blank"> Mais informações </a>';
+    
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(ponto.Location.lat, ponto.Location.long),
+        title: ponto.circuitName + ", " + ponto.Location.locality + " - " + ponto.Location.country,
+        
+        map: map
+    });
+    
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
+
+fetch("http://ergast.com/api/f1/circuits.json?limit=300")
+.then(response => response.json()) // retorna uma promise
+.then(result => {
+    pontos = result;
+    console.log(pontos);
+    initialize();
+    carregarPontos();
+})
+.catch(err => {
+// trata se alguma das promises falhar
+console.error('Failed retrieving information', err);
+});
+
+
